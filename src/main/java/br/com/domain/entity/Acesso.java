51e5d0248254a71f5b315c8389f17f8fcc9f0559
eduardomdh <eduardodiasdesousa28@gmail.com>
2025-09-30 21:14:41 -0300

    atualizaçao

---
+++ src/main/java/br/com/domain/entity/Acesso.java
@@ -0,0 +1,34 @@
+package br.com.domain.entity;
+
+
+
+public class Acesso {
+
+    private String usuario;
+    private String senha;
+
+    //construtor
+    public Acesso(String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    //getters
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    //setters
+    public void setUsuario(){
+        this.usuario = usuario;
+    }
+
+    public void setSenha(){
+        this.senha = senha;
+    }
+}
